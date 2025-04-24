438473c6222afc3aa0aa79115438b7daa6a616d6
mamun2519 <juborajislam46@gmail.com>
2025-04-24 12:15:30 +0600

    added to custom array

--- src/Array/CustomArray.ts
+++ src/Array/CustomArray.ts
@@ -1,1 +1,11 @@
-class CustomArray {}
+const DEFAULT_CAPACITY = 10;
+class CustomArray {
+  constructor(capacity = DEFAULT_CAPACITY) {
+    this.capacity = capacity;
+    this.length = 0;
+    this.array = new Array(capacity);
+  }
+}
+
+const Array = new CustomArray(5);
+console.log(Array);
